10f2513f9611bd7bdd91937647fa138b69d4a9dd
Jose Gabriel Verutti Narvaez <verutti.gabriel@gmail.com>
2024-10-04 16:56:46 -0500

    aplicación funcional sin sharepoint

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
 from config.chatgpt import getChatGPTEmbeddings, client
 from config.pinecone import conectPineconeIndex
-from config.sharepoint import get_sharepoint_context_using_app
 
 from services.chunks import getChunks, getChunksSingleFile
 from services.pinecone import searchDB, updateDB, getAllDocuments, searchDBFilter
-from services.sharepoint import get_files, post_files
 from services.chatgpt import get_answer
 
 
@@ -13,12 +11,6 @@ from services.chatgpt import get_answer
 embeddings = getChatGPTEmbeddings()
 
 db = conectPineconeIndex(embeddings, "rag-gianpi-texts")
-
-ctx = get_sharepoint_context_using_app()
-
-url = "sites/Python_SOS_Doctorado/Documentos%20compartidos"
-
-print(post_files(url,"file.txt", ctx))
 
 def actualizarDB(filename):
 
